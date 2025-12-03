8ee0a7e10e7245877c5af515207b58b2e8b87eef
lisehr <lisehr@users.noreply.github.com>
2025-12-04 00:20:26 +0100

    Add nltk to requirements

--- metis/metric/__init__.py
+++ metis/metric/__init__.py
@@ -1,5 +1,5 @@
 from .metric import Metric
 from .completeness.completeness import Completeness
 from .consistency.consistency import Consistency
-from .uniqueness.attribute_uniqueness import AttributeUniqueness
+from .minimality.column_minimality_duplicateCount import column_minimality_duplicateCount
 from .validity.out_of_vocabulary import OutOfVocabulary
